e5b6cf37943e98a2a5016a1c3905a9669f1008f4
Aaron Marriner <amarriner@amarriner.com>
2016-05-26 11:22:40 -0400

    Fleshing out file structure, getting grunt ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,56 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            all: {
+                files: {
+                    src: [
+                        'Gruntfile.js',
+                        'server.js',
+                        'src/js/**/*.js'
+                    ]
+                }
+            }
+        },
+
+        jsonlint: {
+            all: [
+                'package.json',
+                'bower.json',
+                '.bowerrc'
+            ]
+        },
+
+        watch: {
+            js: {
+                files: [
+                    'src/js/**/*.js',
+                    'src/js/views/**/*.html',
+                    'Gruntfile.js',
+                    'server.js',
+                    '*.json',
+                    '.bowerrc'
+                ],
+                tasks: ['lint']
+            },
+
+            json: {
+                files: [
+                    'package.json',
+                    'bower.json',
+                    '.bowerrc'
+                ],
+                tasks: ['lint']
+            }
+        }
 
     });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jsonlint');
+
+    grunt.registerTask('lint', ['jsonlint', 'jshint']);
 };
---
+++ server.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+}());
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 80;
+
+var environments = {
+    "dev": "src",
+    "prod": "dist"
+};
+
+var env = "dev";
+if (process.argv.length > 2) {
+    if (process.argv[2] === "prod") {
+        env = "prod";
+    }
+}
+
+app.use(express.static(environments[env]));
+
+app.listen(port);
+
+console.log('Environment set to ' + env);
+console.log('Static files served from ' + environments[env] + '/ directory');
+console.log('Server listening on port ' + port);
